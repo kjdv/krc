8606f549cf086b97518c36c5b016a7740ce292e0
Klaas de Vries <klaasjacobdevries@gmail.com>
2018-10-28 17:46:56 +0000

    testing executor

--- test/executor.cc
+++ test/executor.cc
@@ -8,16 +8,48 @@ namespace {
 using namespace std;
 using namespace testing;
 
-TEST(executor, basic_run)
+class executor_test : public Test
 {
-    vector<int> events;
+public:
+  vector<int> events;
+  executor &exec = executor::instance();
 
-    executor::instance().run([&]{
-      for(int i = 0; i < 3; ++i)
+  std::function<void()> emitter(int start, int end, bool interrupt = false)
+  {
+    return [=] {
+      for(int i = start; i < end; ++i)
+      {
         events.push_back(i);
-    });
 
-    EXPECT_THAT(events, ElementsAre(0, 1, 2));
+        if(interrupt)
+          exec.yield();
+      }
+    };
+  }
+};
+
+TEST_F(executor_test, basic_run)
+{
+  exec.run(emitter(0, 3));
+  EXPECT_THAT(events, ElementsAre(0, 1, 2));
+}
+
+TEST_F(executor_test, serial)
+{
+  exec.push(emitter(0, 3));
+  exec.push(emitter(3, 6));
+  exec.run();
+
+  EXPECT_THAT(events, ElementsAre(0, 1, 2, 3, 4, 5));
+}
+
+TEST_F(executor_test, parallel)
+{
+  exec.push(emitter(0, 3, true));
+  exec.push(emitter(3, 6, true));
+  exec.run();
+
+  EXPECT_THAT(events, ElementsAre(0, 3, 1, 4, 2, 5));
 }
 
 }
