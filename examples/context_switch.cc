33d4c3369151a2b52ad2d4ce2523ddb73e7df6f3
Klaas de Vries <klaasjacobdevries@gmail.com>
2018-10-30 23:01:26 +0000

    add example

---
+++ examples/context_switch.cc
@@ -0,0 +1,55 @@
+#include <context.hh>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+using namespace krc;
+
+namespace {
+
+context<>::handle main_ctx, foo_ctx, bar_ctx;
+
+void foo()
+{
+    cout << "enter foo" << " on " << context<>::get_id() << endl;
+    cout << "switching to bar" << endl;
+    context<>::swap(foo_ctx, bar_ctx);
+    cout << "done with foo" << " on " << context<>::get_id() << endl;
+    cout << "switching to main" << endl;
+    context<>::swap(foo_ctx, main_ctx);
+
+    assert(false && "never reached");
+}
+
+void bar()
+{
+    cout << "enter bar" << " on " << context<>::get_id() << endl;
+    cout << "switching to foo" << endl;
+    context<>::swap(bar_ctx, foo_ctx);
+
+    assert(false && "never reached");
+}
+
+}
+
+int main()
+{
+    enum {stack_size = 1<<16};
+
+    main_ctx = context<>::main();
+    foo_ctx = context<>::make(foo, stack_size);
+    bar_ctx = context<>::make(bar, stack_size);
+
+    cout << "switching to foo" << " on " << context<>::get_id() << endl;
+    context<>::swap(main_ctx, foo_ctx);
+    cout << "back to main" << " on " << context<>::get_id() << endl;
+
+    cout << "freeing up" << endl;
+    context<>::free(bar_ctx);
+    context<>::free(foo_ctx);
+    context<>::free(main_ctx);
+
+    cout << "all done" << endl;
+
+    return 0;
+}
