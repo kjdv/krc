7672a95581eb9ef1d7f372efc906e15ad2bcc922
Klaas de Vries <klaasjacobdevries@gmail.com>
2018-11-02 21:12:55 +0000

    fix

--- src/ptr_translate.cc
+++ src/ptr_translate.cc
@@ -8,7 +8,7 @@ static_assert(sizeof(int) * 2 >= sizeof(void *), "you run an unusual system");
 void from_ptr(void *p, int &p1, int &p2)
 {
     int buf[] = {0, 0};
-    mempcpy(buf, &p, 2 * sizeof(int));
+    memcpy(buf, &p, 2 * sizeof(int));
     p1 = buf[0];
     p2 = buf[1];
 }
@@ -17,7 +17,7 @@ void *to_ptr(int p1, int p2)
 {
     int buf[] = {p1, p2};
     void *p;
-    mempcpy(&p, buf, 2 * sizeof(int));
+    memcpy(&p, buf, 2 * sizeof(int));
     return p;
 }
 
