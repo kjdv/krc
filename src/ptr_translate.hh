824286924f1c7ff1474fddf1f1e59317790bfa99
Klaas de Vries <klaasjacobdevries@gmail.com>
2018-11-03 00:46:27 +0000

    nit

--- src/ptr_translate.hh
+++ src/ptr_translate.hh
@@ -4,7 +4,7 @@ namespace krc {
 
 // helper for the deeply unhelpful signature of makecontext, enabling only int arguments to the target function
 
-void from_ptr(void* p, int& p1, int& ptr_as_intsp2);
+void from_ptr(void* p, int& p1, int& p2);
 
 void* to_ptr(int p1, int p2);
 
